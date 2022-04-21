ad5ee9f35f483e937b151cca6d6091c09c7ecd44
bui bao long <blong1102@gmail.com>
2022-04-21 11:47:07 +0700

    sum zero

--- same/index.js
+++ same/index.js
@@ -18,9 +18,40 @@ function same(arr1, arr2) {
     }
   })
 
-  console.log({ hashArr1, hashArr2 })
-
   return count === arr1.length ? true : false
+}
+
+const anagram = (str1, str2) => {
+  if (str1.length !== str2.length) return false
+  if (str1.length === 0 && str2.length === 0) return true
+
+  const str1Obj = str1.split("").reduce((res, curr) => {
+    res[curr] = ++res[curr] || 1
+    return res
+  }, {})
+
+  const str2Obj = str2.split("").reduce((res, curr) => {
+    res[curr] = ++res[curr] || 1
+    return res
+  }, {})
+
+  for (let key in str1Obj) {
+    if (str2Obj[key] !== str1Obj[key]) return false
+  }
+  return true
+}
+
+const sumZero = (array) => {
+  const check = {}
+  const result = []
+
+  array.forEach((element) => {
+    if (check[0 - element]) {
+      result.push([element, check[0 - element]])
+    } else check[element] = element
+  })
+
+  return result.length === 0 ? undefined : result[result.length - 1]
 }
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
@@ -54,6 +85,25 @@ describe("same function", () => {
     assert.equal(same([1, 2, 3], [4, 1, 9]), true)
     assert.equal(same([1, 2, 3], [1, 9]), false)
     assert.equal(same([1, 2, 1], [4, 4, 1]), false)
+  })
+})
+
+describe("have same charactors", () => {
+  it("have same charactors", () => {
+    assert.equal(anagram("", ""), true)
+    assert.equal(anagram("aaz", "zza"), false)
+    assert.equal(anagram("anagram", "nagaram"), true)
+    assert.equal(anagram("rat", "car"), false)
+    assert.equal(anagram("awesome", "awesom"), false)
+    assert.equal(anagram("qwerty", "qeywrt"), true)
+  })
+})
+
+describe("sum zero", () => {
+  it("sum zero", () => {
+    assert.deepEqual(sumZero([-3, -2, -1, 0, 1, 2, 3]), [3, -3])
+    assert.equal(sumZero([-2, 0, 1, 3]), undefined)
+    assert.equal(sumZero([1, 2, 3]), undefined)
   })
 })
 
